e933b8fc7c0a88632e7bde74e04018e2bd40037f
Jean Tinland <jean.tinland@gmail.com>
2025-05-24 14:26:32 +0200

    chore: cleanup obsolete props in post-it.tsx

--- src/components/post-it/post-it.tsx
+++ src/components/post-it/post-it.tsx
@@ -212,7 +212,6 @@ export default function PostIt({
       onDragStart={() => setDragged(true)}
       onDragEnd={updatePostItPosition}
       onBlur={updatePostIt}
-      onResize={handleHeightChange}
       initial={dragging ? undefined : { scale: 0.8, opacity: 0 }}
       animate={dragging ? undefined : { scale: 1, opacity: 1 }}
       exit={{ opacity: 0, scale: 0.8 }}
